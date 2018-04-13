d4f2f1015a47960fc1e5baae21d27b468ae2f2dd
kingLiYang <481767517@qq.com>
2018-04-13 18:05:28 +0800

    1

---
+++ js/common.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+    $('#menu1,#menu2,#menu3').tendina({
+        openCallback: function(clickedEl) {
+            clickedEl.addClass('opened');
+        },
+        closeCallback: function(clickedEl) {
+            clickedEl.addClass('closed');
+        }
+    });
+ 
+	
+	$("#menu li a.firsta").click(function(){
+
+			var status=$(this).next("ul").css("display");
+			var firstchild_a=$(this).find("i");
+			var all_lis=$(this).parents("ul").find("a.firsta i");
+			all_lis.removeClass("xlcddown");
+			all_lis.addClass("xlcd");
+				
+			if(status=="none"){
+				firstchild_a.removeClass("xlcd");
+				firstchild_a.addClass("xlcddown");
+			}else{
+				firstchild_a.removeClass("xlcddown");
+				firstchild_a.addClass("xlcd");	
+			}
+		
+		});
+});
+
+$(function(){
+	
+	$("#menu li.menu-list>ul>li").click(function(){
+		$("#menu li.menu-list>ul>li").find("a").removeClass("specialclass");
+		$(this).find("a",this).addClass("specialclass");
+		
+	})
+})
+	
+
